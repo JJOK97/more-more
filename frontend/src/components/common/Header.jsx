63c6ab2fdf66038c5b0beb5119982dff22d7cc98
JinSeok <spdlqjanf@naver.com>
2024-09-10 18:51:58 +0900

    design: Header, Footer (S11P21A605-343)

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-import '@/assets/Common/header.css';
+import '@/assets/css/common/header.css';
+import logo from '@/assets/img/header/More More.svg';
+import search from '@/assets/img/header/search.svg';
+import user from '@/assets/img/header/user.svg';
+import bell from '@/assets/img/header/bell.svg';
 
 const Header = () => {
-	return <header></header>;
+	return (
+		<header>
+			<div className="logo">
+				<img src={logo}></img>
+			</div>
+			<div className="userNavbar">
+				<img src={search}></img>
+				<img src={user}></img>
+				<img src={bell}></img>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import Header from '@/components/common/Header';
+import Footer from '@/components/common/Footer';
+
 import '@/assets/css/common/Main.css';
 
 const Main = () => {
@@ -12,8 +15,12 @@ const Main = () => {
 
 	return (
 		<div className="main">
-			<h1>Welcome to Main Page</h1>
-			<button onClick={handleLogin}>Go to Login</button>
+			<Header />
+			<div className="body">
+				<h1>Welcome to Main Page</h1>
+				<button onClick={handleLogin}>Go to Login</button>
+			</div>
+			<Footer />
 		</div>
 	);
 };
