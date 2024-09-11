d1373a77fd4c06238276a233f60d12b3666029b2
권주안 <wndks6450@gmail.com>
2024-09-11 14:58:58 +0900

    design: control size for header,footer (S11P21A605-295)

--- frontend/src/router/appRouter.jsx
+++ frontend/src/router/appRouter.jsx
@@ -1,32 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '@/assets/css/common/appRouter.css';
 import Main from '@/pages/Main';
 import Login from '@/pages/Login/Login';
 import Profile from '@/pages/User/Profile';
 import Feed from '@/pages/feed/Feed';
+import Header from '@/components/common/GroupHeader';
+import Footer from '@/components/common/GroupFooter';
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Main />}
-                />
-                <Route
-                    path="/login"
-                    element={<Login />}
-                />
-                <Route
-                    path="/profile"
-                    element={<Profile />}
-                />
-                <Route
-                    path="/feed"
-                    element={<Feed />}
-                />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<Header />
+			<main>
+				<Routes>
+					<Route
+						path="/"
+						element={<Main />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="/profile"
+						element={<Profile />}
+					/>
+					<Route
+						path="/feed"
+						element={<Feed />}
+					/>
+				</Routes>
+			</main>
+			<Footer />
+		</Router>
+	);
 };
 
 export default AppRouter;
