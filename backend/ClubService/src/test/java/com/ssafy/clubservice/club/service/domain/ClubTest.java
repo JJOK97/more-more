328177acc7570abc7d0cdd10a595e3d8c5380613
jumsun <pjsw8361@gmail.com>
2024-09-11 09:54:30 +0900

    feat: 모임 서비스 이름 / 회비 수정 테스트 코드 추가

--- backend/ClubService/src/test/java/com/ssafy/clubservice/club/service/domain/ClubTest.java
+++ backend/ClubService/src/test/java/com/ssafy/clubservice/club/service/domain/ClubTest.java
@@ -43,6 +43,63 @@ class ClubTest {
         assertThat(club.getClubImage()).isEqualTo("test");
     }
 
-
+    @DisplayName("클럽은 이름을 변경할 수 있다.")
+    @Test
+    void updateName(){
+        Club club = Club.builder()
+                .clubId(1L)
+                .clubCode("11")
+                .dues(10L)
+                .clubName("11")
+                .build();
+        Club updateData = Club.builder()
+                .clubId(1L)
+                .dues(10L)
+                .clubName("22")
+                .build();
+        club.update(updateData);
+        assertThat(club.getClubId()).isEqualTo(1L);
+        assertThat(club.getDues()).isEqualTo(10L);
+        assertThat(club.getClubName()).isEqualTo("22");
+    }
+    @DisplayName("클럽 회비를 변경할 수 있다.")
+    @Test
+    void updateDues(){
+        Club club = Club.builder()
+                .clubId(1L)
+                .clubCode("11")
+                .dues(10L)
+                .clubName("11")
+                .build();
+        Club updateData = Club.builder()
+                .clubId(1L)
+                .dues(20L)
+                .clubName("11")
+                .build();
+        club.update(updateData);
+        assertThat(club.getClubId()).isEqualTo(1L);
+        assertThat(club.getDues()).isEqualTo(20L);
+        assertThat(club.getClubName()).isEqualTo("11");
+    }
+    @DisplayName("클럽 이름과 회비를 변경할 수 있다.")
+    @Test
+    void updateNameAndDues(){
+        Club club = Club.builder()
+                .clubId(1L)
+                .dues(10L)
+                .clubCode("11")
+                .clubName("11")
+                .build();
+        Club updateData = Club.builder()
+                .clubId(1L)
+                .dues(20L)
+                .clubCode("11")
+                .clubName("22")
+                .build();
+        club.update(updateData);
+        assertThat(club.getClubId()).isEqualTo(1L);
+        assertThat(club.getDues()).isEqualTo(20L);
+        assertThat(club.getClubName()).isEqualTo("22");
+    }
 }
 
