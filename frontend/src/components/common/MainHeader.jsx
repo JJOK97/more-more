e3ca5eb4ccbc4ca592113ee77aae10c72e68bf0d
JinSeok <spdlqjanf@naver.com>
2024-09-11 10:50:24 +0900

    design: 메인 헤더 디자인 (S11P21A605-343)

---
+++ frontend/src/components/common/MainHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import '@/assets/css/common/header.css';
+import logo from '@/assets/img/header/text-logo.svg';
+import search from '@/assets/img/header/search.svg';
+import user from '@/assets/img/header/user.svg';
+import bell from '@/assets/img/header/bell.svg';
+
+const Header = () => {
+	return (
+		<header>
+			<div className="menu">
+				<div className="logo">
+					<img src={logo}></img>
+				</div>
+				<div className="userNavbar">
+					<img src={search}></img>
+					<img src={user}></img>
+					<img src={bell}></img>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
