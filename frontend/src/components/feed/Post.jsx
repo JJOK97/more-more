403cf903aa01343083e7dd148427b55f34dc9453
권주안 <wndks6450@gmail.com>
2024-09-11 14:06:56 +0900

    design: component placement (S11P21A605-284)

---
+++ frontend/src/components/feed/Post.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Post = ({ post }) => {
+	return (
+		<div className="feed-area">
+			<div className="feed-head-area">
+				<div className="feed-profile-area">
+					<img
+						className="feed-profile-image"
+						src={post.userProfile}
+						alt="프로필"
+					/>
+					<div className="feed-profile-data">
+						<div className="feed-profile-name">{post.userName}</div>
+						<div className="feed-profile-date">{post.date}</div>
+					</div>
+				</div>
+				<div className="feed-account-history">#{post.accountHistory}</div>
+			</div>
+			<div className="feed-post-area">
+				<div className="feed-post-content">{post.postContent}</div>
+				<img
+					className="feed-post-image"
+					src={post.postImage}
+					alt="사진"
+				/>
+			</div>
+			<div className="feed-att-area">
+				<div className="feed-att-like">
+					<img className="feed-att-like-icon" src="/feed/Heart.svg" alt="좋아요 아이콘" />
+					<div className="feed-att-like-count">{post.likeCount}</div>
+				</div>
+				<div className="feed-att-comment">
+					<img className="feed-att-comment-icon" src="/feed/chat.svg" alt="댓글 아이콘" />
+					<div className="feed-att-comment-count">{post.commentCount}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Post;
--- frontend/src/pages/feed/Feed.jsx
+++ frontend/src/pages/feed/Feed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '@/assets/css/feed/Feed.css';
+import Post from '@/components/feed/Post';
 
 const Feed = () => {
 	const posts = [
@@ -7,46 +8,52 @@ const Feed = () => {
 			postId: 1,
 			userId: 1,
 			userName: '옥진석',
-			userProfile: '/feed/jinsukProfile.png',
+			userProfile: '/feed/profile_icon2.png',
 			date: '2024.09.10',
 			accountHistory: '2022.05.19 서브웨이 역삼점',
 			postContent:
 				'이런 눈빛을 한 지은이의 정면 얼굴을 마주할 때마다, 영영 익숙해지지 않을 것 같은 새로운 종류의 박자로 심장이 달음질 친다. 다람쥐 헌 쳇바퀴에 타고파.',
 			postImage: 'feed/회식.webp',
+			likeCount: 1,
+			commentCount: 1,
+		},
+        {
+			postId: 2,
+			userId: 2,
+			userName: '박지환',
+			userProfile: '/feed/profile_icon3.png',
+			date: '2024.09.10',
+			accountHistory: '2022.05.19 서브웨이 역삼점',
+			postContent:
+				'이런 눈빛을 한 지은이의 정면 얼굴을 마주할 때마다, 영영 익숙해지지 않을 것 같은 새로운 종류의 박자로 심장이 달음질 친다. 다람쥐 헌 쳇바퀴에 타고파.',
+			postImage: 'feed/회식.webp',
+			likeCount: 1,
+			commentCount: 1,
+		},
+		{
+			postId: 3,
+			userId: 3,
+			userName: '옥진석',
+			userProfile: '/feed/profile_icon2.png',
+			date: '2024.09.10',
+			accountHistory: '2022.05.19 서브웨이 역삼점',
+			postContent:
+				'이런 눈빛을 한 지은이의 정면 얼굴을 마주할 때마다, 영영 익숙해지지 않을 것 같은 새로운 종류의 박자로 심장이 달음질 친다. 다람쥐 헌 쳇바퀴에 타고파.',
+			postImage: 'feed/회식.webp',
+			likeCount: 1,
+			commentCount: 1,
 		},
 	];
 	return (
-		<div>
+		<div className='group-feed'>
 			<img
 				className="group-background-image"
-				src='/feed/배경사진.png'
+				src="/feed/배경사진.png"
 				alt="배경사진"
 			/>
-			<div className='feed-container'>
+			<div className="feed-container">
 				{posts.map((post) => (
-					<div key={post.postId} className='feed-area'>
-						<div className='feed-profile-area'>
-							{/* 프로필 */}
-							<img
-								className="feed-profile-image"
-								src={post.userProfile}
-								alt="프로필"
-							/>
-							<div className='feed-profile-data'>
-								<div className='feed-profile-name'>{post.userName}</div>
-								<div className='feed-profile-date'>{post.date}</div>
-							</div>
-						</div>
-						<div className='feed-post-area'>
-							{/* 게시글 */}
-							<div className='feed-post-content'>{post.postContent}</div>
-							<img
-								className="feed-post-image"
-								src={post.postImage}
-								alt="사진"
-							/>
-						</div>
-					</div>
+					<Post key={post.postId} post={post} />
 				))}
 			</div>
 		</div>
