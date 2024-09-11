3a8cd5375ff3615cfd1f66896c6526409002c1af
JinSeok <spdlqjanf@naver.com>
2024-09-11 12:40:30 +0900

    design: groupHeader 디자인 (S12P21A605-343)

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import Header from '@/components/common/MainHeader';
+import Header from '@/components/common/GroupHeader';
 import Footer from '@/components/common/GroupFooter';
 
 import '@/assets/css/common/Main.css';
