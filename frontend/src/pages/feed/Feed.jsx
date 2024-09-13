9cce519752e64f49a63d497c8fafcab319f60967
권주안 <wndks6450@gmail.com>
2024-09-13 14:33:46 +0900

    feat: add Link from feed to post (S11P21A605-297)

--- frontend/src/pages/feed/Feed.jsx
+++ frontend/src/pages/feed/Feed.jsx
@@ -1,26 +1,36 @@
-
 import React from 'react';
 import '@/assets/css/feed/Feed.css';
 import PostView from '@/components/postView/PostView';
-import datas from "@/pages/feed/data.json"
+import datas from '@/pages/feed/data.json';
+import { Link } from 'react-router-dom';
 
 const Feed = () => {
-    const posts = datas.posts;
-    return (
-        <div className='group-feed'>
-            <img
-                className="group-background-image"
-                src="/feed/배경사진.png"
-                alt="배경사진"
-            />
-            <div className="feed-container">
-                {posts ? posts.map((post) => (
-                    <PostView key={post.postId} post={post} />
-                )) : ''}
-            </div>
-            <img className='feed-write-btn' src='/feed/edit.svg' />
-        </div>
-    );
+	const posts = datas.posts;
+	return (
+		<div className="group-feed">
+			<img
+				className="group-background-image"
+				src="/feed/배경사진.png"
+				alt="배경사진"
+			/>
+			<div className="feed-container">
+				{posts
+					? posts.map((post) => (
+							<PostView
+								key={post.postId}
+								post={post}
+							/>
+					  ))
+					: ''}
+			</div>
+			<Link to={`/create-post`}>
+				<img
+					className="feed-write-btn"
+					src="/feed/edit.svg"
+				/>
+			</Link>
+		</div>
+	);
 };
 
 export default Feed;
