cf15bedddc3858c4a2bd04b78e529c0ecd2cbc82
이재성 <commonhm@gmail.com>
2024-09-13 14:41:38 +0900

    design: group account deposit list ui (S11P21A605-300)

---
+++ frontend/src/components/groupAccount/GroupAccountDepositList.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '@/assets/css/groupAccount/GroupAccount.css';
+
+const GroupAccountDepositList = () => {
+	return (
+		<div>
+			<div className="deposit-list-date">9월 5일</div>
+			<div className="deposit-detail-one">
+				<div className="deposit-list-place-price">
+					<div className="deposit-list-place">지에스25 디엑스랩점</div>
+					<div className="deposit-list-price">-2,800원</div>
+				</div>
+				<div className="deposit-list-time-balance">
+					<div className="deposit-list-time">18:39</div>
+					<div className="deposit-list-balance">3,410,000원</div>
+				</div>
+			</div>
+			<div className="deposit-detail-one">
+				<div className="deposit-list-place-price">
+					<div className="deposit-list-place">지에스25 디엑스랩점</div>
+					<div className="deposit-list-price">-2,800원</div>
+				</div>
+				<div className="deposit-list-time-balance">
+					<div className="deposit-list-time">18:39</div>
+					<div className="deposit-list-balance">3,410,000원</div>
+				</div>
+			</div>
+			<div className="deposit-detail-one">
+				<div className="deposit-list-place-price">
+					<div className="deposit-list-place">지에스25 디엑스랩점</div>
+					<div className="deposit-list-price">-2,800원</div>
+				</div>
+				<div className="deposit-list-time-balance">
+					<div className="deposit-list-time">18:39</div>
+					<div className="deposit-list-balance">3,410,000원</div>
+				</div>
+			</div>
+			<div className="deposit-detail-one">
+				<div className="deposit-list-place-price">
+					<div className="deposit-list-place">지에스25 디엑스랩점</div>
+					<div className="deposit-list-price">-2,800원</div>
+				</div>
+				<div className="deposit-list-time-balance">
+					<div className="deposit-list-time">18:39</div>
+					<div className="deposit-list-balance">3,410,000원</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default GroupAccountDepositList;
--- frontend/src/pages/groupAccount/GroupAccount.jsx
+++ frontend/src/pages/groupAccount/GroupAccount.jsx
@@ -1,12 +1,14 @@
 // 여기에 import 시켜서 화면 렌더링 시키기
 // 컴포넌트들이 다 합쳐지는 페이지
 import AccountBalance from '@/components/groupAccount/GroupAccountBalance';
+import GroupAccountDepositWithdrawalDetails from '@/components/groupAccount/GroupAccountDepositWithDrawalDetails';
 import '@/assets/css/groupAccount/GroupAccount.css';
 
 const GroupAccount = () => {
 	return (
-		<div className="AccountArea">
+		<div className="account-area">
 			<AccountBalance />
+			<GroupAccountDepositWithdrawalDetails />
 		</div>
 	);
 };
