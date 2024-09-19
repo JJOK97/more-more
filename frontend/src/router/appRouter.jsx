72d8eb3e9bcaa8f97cabaec7d27885c290a9ef23
이재성 <commonhm@gmail.com>
2024-09-19 16:42:15 +0900

    hotfix: 그룹 계좌 화면 route 설정 (S11P21A605-393)

--- frontend/src/router/appRouter.jsx
+++ frontend/src/router/appRouter.jsx
@@ -19,14 +19,42 @@ const AppRouter = () => {
 			<Header />
 			<main>
 				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/feed" element={<Feed />} />
-					<Route path="/create-post" element={<CreatePost />} />
-					<Route path="/groupaccount" element={<GroupAccount />} />
-					<Route path="/group/:groupId" element={<Feed />} />
-					<Route path="/group/:groupId/:postId" element={<Post />} />
+					<Route
+						path="/"
+						element={<Main />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="/profile"
+						element={<Profile />}
+					/>
+					<Route
+						path="/feed"
+						element={<Feed />}
+					/>
+					<Route
+						path="/create-post"
+						element={<CreatePost />}
+					/>
+					<Route
+						path="/groupaccount"
+						element={<GroupAccount />}
+					/>
+					<Route
+						path="/group/:groupId"
+						element={<Feed />}
+					/>
+					<Route
+						path="/group/:groupId/:postId"
+						element={<Post />}
+					/>
+					<Route
+						path="/deposit-status"
+						element={<GroupAccountDepositStatus />}
+					/>
 				</Routes>
 			</main>
 			<Footer />
