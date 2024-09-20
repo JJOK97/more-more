b8c509e1df8372ac847d04a940dea014437260f9
이재성 <commonhm@gmail.com>
2024-09-20 14:55:32 +0900

    design: 계좌 송금 상단 정보 컴포넌트 (S11P21A605-396) #in-progress

---
+++ frontend/src/components/groupAccount/TransferInfo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import back from '@/assets/img/account/back.svg';
+
+const TransferInfo = () => {
+	return (
+		<div className="transfer-info">
+			<div>
+				<img src={back}></img>
+			</div>
+			<div className="transfer-account-info">
+				<div className="transer-my-account">
+					<div className="from-my-account">내 하나은행 입출금 계좌에서</div>
+					<div className="transfer-balance">잔액 1,000,000원</div>
+				</div>
+				<div className="transfer-group-account">
+					<div className="to-group-account">가족모임 계좌로</div>
+					<div className="transfer-account">싸피 뱅크 3333-02-123456</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TransferInfo;
---
+++ frontend/src/components/groupAccount/TransferKeypad.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TransferKeypad = () => {
+	return <div>TransferKeypad</div>;
+};
+
+export default TransferKeypad;
