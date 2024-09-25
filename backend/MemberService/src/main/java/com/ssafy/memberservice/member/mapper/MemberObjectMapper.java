64c40fb9351f549ec9f28db1db0fe3f45a9e3f8d
ssh2957 <ssh2957@naver.com>
2024-09-25 10:39:10 +0900

    feat: member swager+S3 make

---
+++ backend/MemberService/src/main/java/com/ssafy/memberservice/member/mapper/MemberObjectMapper.java
@@ -0,0 +1,12 @@
+package com.ssafy.memberservice.member.mapper;
+
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberObjectMapper {
+
+}
