47da83ab9af68f09925b11ec6722d79618f9a106
JinSeok <spdlqjanf@naver.com>
2024-09-26 10:01:36 +0900

    feat: footer router 설정 (S11P21A605-463)

--- frontend/src/components/common/GroupFooter.jsx
+++ frontend/src/components/common/GroupFooter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import '@/assets/css/common/groupFooter.css';
 
@@ -10,30 +11,56 @@ import calendarOn from '@/assets/img/common/groupFooter/calendar-on.svg';
 import calendarOff from '@/assets/img/common/groupFooter/calendar-off.svg';
 import settingOn from '@/assets/img/common/groupFooter/setting-on.svg';
 import settingOff from '@/assets/img/common/groupFooter/setting-off.svg';
-import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+	const navigate = useNavigate();
 	const location = useLocation();
+
+	// 특정 경로에서는 Footer를 렌더링하지 않음
 	if (
-		location.pathname === '/' ||
-		location.pathname == '/login' ||
-		location.pathname == '/signup' ||
-		location.pathname == '/profile' ||
-		location.pathname == '/notice' ||
-		location.pathname == '/create' ||
-		location.pathname == '/group/:groupId/account/transfer' ||
-		location.pathname == '/group/:groupId/account/transfer-question' ||
-		location.pathname == '/group/:groupId/account/transfer-check'
-	)
+		['/', '/login', '/signup', '/profile', '/notice', '/create'].includes(location.pathname) ||
+		location.pathname.match(/^\/group\/\d+\/account\/transfer/) // 동적 경로 패턴 처리
+	) {
 		return null;
+	}
+
+	// 정규식을 사용하여 동적 경로 비교
+	const isBoardActive = /^\/group\/\d+$/.test(location.pathname);
+	const isAccountActive = /^\/group\/\d+\/account$/.test(location.pathname);
+	const isCalendarActive = /^\/group\/\d+\/schedule$/.test(location.pathname);
+	const isSettingActive = /^\/group\/\d+\/info$/.test(location.pathname);
+
+	const handleNavigation = (path) => {
+		// groupId를 추출하여 경로에 반영
+		const groupId = location.pathname.match(/^\/group\/(\d+)/)?.[1];
+		if (groupId) {
+			navigate(path.replace(':groupId', groupId));
+		}
+	};
 
 	return (
 		<footer>
 			<div className="navbar">
-				<img src={boardOff}></img>
-				<img src={accountOff}></img>
-				<img src={calendarOff}></img>
-				<img src={settingOff}></img>
+				<img
+					src={isBoardActive ? boardOn : boardOff}
+					alt="Board"
+					onClick={() => handleNavigation('/group/:groupId')}
+				/>
+				<img
+					src={isAccountActive ? accountOn : accountOff}
+					alt="Account"
+					onClick={() => handleNavigation('/group/:groupId/account')}
+				/>
+				<img
+					src={isCalendarActive ? calendarOn : calendarOff}
+					alt="Calendar"
+					onClick={() => handleNavigation('/group/:groupId/schedule')}
+				/>
+				<img
+					src={isSettingActive ? settingOn : settingOff}
+					alt="Setting"
+					onClick={() => handleNavigation('/group/:groupId/info')}
+				/>
 			</div>
 		</footer>
 	);
