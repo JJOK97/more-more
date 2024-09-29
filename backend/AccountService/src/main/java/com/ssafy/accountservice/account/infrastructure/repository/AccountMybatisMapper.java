16c42272d364e41991cdf8cd5732b5cdccda3de8
박지환 <bat522@naver.com>
2024-09-29 22:05:00 +0900

    feat: account create

---
+++ backend/AccountService/src/main/java/com/ssafy/accountservice/account/infrastructure/repository/AccountMybatisMapper.java
@@ -0,0 +1,9 @@
+package com.ssafy.accountservice.account.infrastructure.repository;
+
+import com.ssafy.accountservice.account.infrastructure.repository.entity.AccountEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AccountMybatisMapper {
+    void insertAccount(AccountEntity accountEntity);
+}
