8fd76ed4b5c7aca3dc59d2270873a9ba0947bb23
jumsun <pjsw8361@gmail.com>
2024-10-07 11:30:52 +0900

    config: accountService 환경 변수를 yaml 파일로 변경

--- backend/AccountService/src/main/java/com/ssafy/accountservice/account/service/domain/AccountUtils.java
+++ backend/AccountService/src/main/java/com/ssafy/accountservice/account/service/domain/AccountUtils.java
@@ -1,20 +1,24 @@
 package com.ssafy.accountservice.account.service.domain;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 
 public class AccountUtils {
+    @Value("${ssafy.api.key}")
+    private static String API_KEY;
+    @Value("${ssafy.user.key}")
+    private static String USER_KEY;
+    @Value("${ssafy.account.no}")
+    private static String ACCOUNT_NO;
     public static String getApiKey() {
-        Dotenv dotenv = Dotenv.load();
-        return dotenv.get("API_KEY");
+        return API_KEY;
     }
 
     public static String getUserKey() {
-        Dotenv dotenv = Dotenv.load();
-        return dotenv.get("USER_KEY");
+        return USER_KEY;
     }
 
     public static String getAccountNo() {
-        Dotenv dotenv = Dotenv.load();
-        return dotenv.get("ACCOUNT_NO");
+        return ACCOUNT_NO;
     }
 }
