b6d2cd83a38788e46da6d0336002f9641665944b
JinSeok <spdlqjanf@naver.com>
2024-10-08 16:49:37 +0900

    feat: 공통 컴포넌트 경로 수정 (S11P21A605-628)

--- frontend/src/components/common/MainHeader.jsx
+++ frontend/src/components/common/MainHeader.jsx
@@ -16,15 +16,24 @@ const Header = () => {
 		<header className="common-header">
 			<div className="menu">
 				<div className="logo">
-					<img src={logo} alt="Logo" />
+					<img
+						src={logo}
+						alt="Logo"
+					/>
 				</div>
 				<div className="userNavbar">
 					<Link to={'/profile'}>
-						<img src={user} alt="Profile" />
+						<img
+							src={user}
+							alt="Profile"
+						/>
 					</Link>
 					<div className="notification-icon-wrapper">
 						<Link to={'/notice'}>
-							<img src={bell} alt="Notifications" />
+							<img
+								src={bell}
+								alt="Notifications"
+							/>
 							{isUnreadNotice && <div className="red-dot" />}
 						</Link>
 					</div>
