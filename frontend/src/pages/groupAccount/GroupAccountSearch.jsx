1f4b690228264d55a395a2c12d9c1c76e168ad01
권주안 <wndks6450@gmail.com>
2024-10-09 22:39:50 +0900

    design : 메인화면 css 수정

--- frontend/src/pages/groupAccount/GroupAccountSearch.jsx
+++ frontend/src/pages/groupAccount/GroupAccountSearch.jsx
@@ -4,10 +4,12 @@ import SearchBar from '@/components/groupAccount/SearchBar';
 import GroupAccountDepositList from '@/components/groupAccount/GroupAccountDepositList';
 import useGroupName from '@/store/useGroupName';
 import { useParams } from 'react-router-dom';
+import { getDatas } from '../feed/getData';
 
 const GroupAccountSearch = () => {
 	const { setGroupName } = useGroupName();
 	const { groupId } = useParams(); // URL에서 groupId를 추출
+	const [groupInfo, setGroupInfo] = useState(null);
 	const [selectedDate, setSelectedDate] = useState(null);
 	const [searchTerm, setSearchTerm] = useState('');
 
@@ -15,9 +17,26 @@ const GroupAccountSearch = () => {
 		setSearchTerm(term);
 	};
 
+	// 그룹 정보를 불러오는 useEffect
 	useEffect(() => {
-		setGroupName(groupId);
-	}, []);
+		const getGroupInfo = async () => {
+			try {
+				const url = `https://j11a605.p.ssafy.io/api/club/${groupId}`;
+				const data = await getDatas(url);
+				setGroupInfo(data);
+			} catch (error) {
+				console.error('Error fetching group info:', error);
+			}
+		};
+		getGroupInfo();
+	}, [groupId]);
+
+	// groupInfo가 업데이트될 때, groupName 상태를 업데이트
+	useEffect(() => {
+		if (groupInfo && groupInfo.clubName) {
+			setGroupName(groupInfo.clubName);
+		}
+	}, [groupInfo, setGroupName]);
 
 	return (
 		<div className="group-account-search-area">
