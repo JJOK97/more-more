b2f5154fcb52f7b47180ceccb351444d964f05f7
JinSeok Ock <spdlqjanf@naver.com>
2024-10-10 15:50:50 +0900

    Feat: FCM 설정 (S11P21A605-639)

--- backend/NotificationService/src/main/java/com/ssafy/notificationservice/global/client/MemberServiceClient.java
+++ backend/NotificationService/src/main/java/com/ssafy/notificationservice/global/client/MemberServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "member-service", url = "https://j11a605.p.ssafy.io/api/member")
+@FeignClient(name = "member-service", url = "https://j11a605.p.ssafy.io")
 public interface MemberServiceClient {
 
     @GetMapping("/api/member/{userId}/fcm-token")
